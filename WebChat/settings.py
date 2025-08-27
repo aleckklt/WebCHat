ee3b72c7f7265b21fd26a1bf17ca17f15153a9db
aleckklt <aleckkouletio@gmail.com>
2025-08-27 09:18:03 +0100

    gestion propre du projet dans systeme de notificaions

--- WebChat/settings.py
+++ WebChat/settings.py
@@ -63,8 +63,11 @@ ASGI_APPLICATION = 'WebChat.asgi.application'
 
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer"
-    }
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
 }
 
 DATABASES = {
